48957ddc15204cde0c442dc8de887f9b2aae8a8c
Taylor Ren <taylor.ren@gmail.com>
2023-02-12 16:50:06 +0800

    Click refresh to change a random book

--- src/widget/LatestBook.svelte
+++ src/widget/LatestBook.svelte
@@ -10,10 +10,10 @@
             {book.title}
         </h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-            【{book.region}】<a href="/book/author/{book.author}" class="hover:bg-sky-700">{book.author}</a> 著{#if book.translated} | {book.copyrighter} 译{/if}<br/>
+            【{book.region}】<a href="/book/author/{book.author}" class="hover:bg-sky-700">{book.author}</a> 著{#if book.translated}&nbsp;| {book.copyrighter} 译{/if}<br/>
             收录于：{book.purchdate}
         </p>
-        <Button href="/books/{book.bookid}.html">
+        <Button href="/books/{book.bookid}.html" color="blue">
             详细信息
         </Button>
     </Card>
--- src/widget/RandomBook.svelte
+++ src/widget/RandomBook.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
-    import { Card, Button, Badge } from 'flowbite-svelte';
-    import { page } from '$app/stores';
-    import type { Book } from "../lib/Book";
-    const book:Book=$page.data.randomBook;
+  import { Card, Button, Badge, ButtonGroup, Tooltip } from "flowbite-svelte";
+  import { page } from "$app/stores";
+  import { BookManager } from "../lib/BookManager";
+
+  let book = $page.data.randomBook;
+
+  async function refresh() {
+    const bm = new BookManager();
+    let rb = await bm.getRandomBook();
+
+    book = rb[0];
+  }
 </script>
+
 <div>
-    <Card img="{book.img}">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {book.title}
-        </h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-            【{book.region}】<a href="/book/author/{book.author}" class="hover:bg-sky-700">{book.author}</a> 著{#if book.translated} | {book.copyrighter} 译{/if}<br/>
-            收录于：{book.purchdate}，访问量：<Badge color="red">{book.vc.toLocaleString("en-US")}</Badge>
-        </p>
-        <Button>详细信息</Button>
-            
-    </Card>
-    
+  <Card img={book.img}>
+    <h5
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+      {book.title}
+    </h5>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+      【{book.region}】<a
+        href="/book/author/{book.author}"
+        class="hover:bg-sky-700">{book.author}</a
+      >
+      著{#if book.translated}&nbsp;| {book.copyrighter} 译{/if}<br />
+      收录于：{book.purchdate}，访问量：<Badge color="red"
+        >{book.vc.toLocaleString("en-US")}</Badge
+      >
+    </p>
+    <ButtonGroup>
+    <Button color="blue">详细信息</Button>
+    <Button on:click={refresh}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+      </svg></Button>
+      <Tooltip placement="right">随机换一本</Tooltip>
+    </ButtonGroup>
+  </Card>
 </div>
