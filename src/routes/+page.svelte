c9c05c42e8ea724c60fea5474bfef73161b3e92c
Taylor Ren <taylor.ren@gmail.com>
2023-09-14 16:31:21 +0800

    Tailwind CSS and Flowbite-Svelte support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Alert, Badge } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Info alert!</span>
+		Change a few things up and try submitting again.
+	</Alert>
+
+	<Badge>Default</Badge>
+	<Badge color="dark">Dark</Badge>
+	<Badge color="red">Red</Badge>
+	<Badge color="green">Green</Badge>
+	<Badge color="yellow">Yellow</Badge>
+	<Badge color="indigo">Indigo</Badge>
+	<Badge color="purple">Purple</Badge>
+	<Badge color="pink">Pink</Badge>
+</div>
